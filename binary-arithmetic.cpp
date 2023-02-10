ae4f2733bd285adb97ec10bc9e3d8d106378b027
sansahoo <sansahoo@equinix.com>
2023-02-10 14:58:01 +0530

    added functiuonality

---
+++ binary-arithmetic.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void add(int a,int b)
+{
+    
+}
+
+void sub(int a,int b)
+{
+
+}
+
+//null
+void mul(int a, int b)
+{
+
+}
+// basic layout of code 
+int main()
+{
+    return 0 ;
+}
